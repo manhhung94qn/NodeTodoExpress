865e04861a1619473e466dc1f380cb7250a0b585
Manh Hung <manhhung94qn@gmail.com>
2019-10-25 01:15:13 +0700

    edit tods

--- api/controllers/setupController.js
+++ api/controllers/setupController.js
@@ -1,1 +1,23 @@
-let Todos = require("");
+let Todos = require("../models/todoModel");
+
+module.exports = function(app){
+    app.get("/api/setupTodos", function(req,res){
+        let seedTodos = [
+            {
+                text:"Học Node.js",
+                isDone: false,
+            },
+            {
+                text:"Học Vue.js",
+                isDone: false,
+            },
+            {
+                text:"Viết ứng dụng hoàn chỉnh",
+                isDone: false,
+            },
+        ]
+        Todos.create(seedTodos, function(err, results){
+            res.send(results);
+        })
+    })
+}
