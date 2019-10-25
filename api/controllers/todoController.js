7d0d39088dc9cf6bc100381cb12b42244c01d1ef
Hung <manhhung94qn@gmail.com>
2019-10-25 08:40:18 +0700

    x

---
+++ api/controllers/todoController.js
@@ -0,0 +1,26 @@
+let Todos = require("../models/todoModel");
+
+function getTodos(res){
+    Todos.find(function(err, result){
+        if(err){
+            res.send(err)
+        } else {
+            res.send(result);
+        }
+    });
+}
+
+module.exports = function(app){
+    app.get("/api/todos", function(req,res){
+        getTodos(res);
+    });
+    app.get("/api/todo/:id", function(req,res){
+        Todos.findById({_id: req.params.id}, function(err, result){
+            if(err){
+                res.send(err)
+            } else {
+                res.send(result);
+            }
+        });
+    });    
+}
